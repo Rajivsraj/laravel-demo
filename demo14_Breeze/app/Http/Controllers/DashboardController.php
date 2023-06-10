007f52534383bfa77871d1640d1a0644ae413b09
Rajiv <rajivsraj78@gmail.com>
2023-06-10 11:16:30 +0530

    Post show userwise

--- demo14_Breeze/app/Http/Controllers/DashboardController.php
+++ demo14_Breeze/app/Http/Controllers/DashboardController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     function post_list(){
-        $posts = Post::all();
+        // Usser wise post only
+        $posts = Auth::user()->post;
         
+
+        // All Posts
+        // $posts = Post::all();
         return view("dashboard", ["posts"=>$posts]);
     }
 }
